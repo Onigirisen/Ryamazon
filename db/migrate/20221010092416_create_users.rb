3381e68892e67350820e4b1c9c60047e151772d6
Ryan Kok <3nayrkok5@gmail.com>
2022-10-13 13:57:48 -0400

    fix migration files

--- db/migrate/20221010092416_create_users.rb
+++ db/migrate/20221010092416_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email null: false
-      t.string :name null: false
-      t.integer :phone_number null: false
-      t.string :password_digest null: false
-      t.string :session_token null: false
+      t.string :email, null: false
+      t.string :name, null: false
+      t.integer :phone_number, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
 
       t.timestamps
     end
