d87a38b074b12cb8e9d49e17b72ff9248a79beb0
Ryan Kok <3nayrkok5@gmail.com>
2022-10-19 11:27:37 -0400

    fixed gemfile for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,10 @@ class User < ApplicationRecord
   
   before_validation :ensure_session_token
  
-  has_one :cart
+  has_many :carts
+  has_many :cart_items,
+  through: :carts,
+  source: :product
 
   def self.find_by_credentials(credential, password)
     field = credential =~ URI::MailTo::EMAIL_REGEXP ? :email : :name
